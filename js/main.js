005120ebac8e5c8599685bf03bf18e2e96217ff8
Nozomi MATSUZAWA <matsuzawa@ueda.info.waseda.ac.jp>
2017-07-25 08:15:45 +0900

    Add function getting canvas and drawing red box.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ function main() {
     chr(document.getElementById('prog-chr').text);
     chr.init();
 }
+
+
+// Native function called from CHR
+
+function getCvs(id) {
+    var cvs = document.getElementById(id);
+    var ctx = cvs.getContext('2d');
+    chr.cvs(id,cvs);
+    chr.ctx(id,ctx);
+}
+
+function drawRedBox(ctx, x, y, w, h) {
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(x,y,w,h);
+}
